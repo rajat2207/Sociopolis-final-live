4445fb87e514c9614a328b85eca7b2b7d511a0c4
rajat2207 <66623568+rajat2207@users.noreply.github.com>
2020-08-06 14:59:35 +0530

    Fetch Posts From API

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-function App() {
-  return <div className="App">App</div>;
+import { fetchPosts } from '../actions/posts';
+
+class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(fetchPosts());
+  }
+
+  render() {
+    console.log('PROPS', this.props);
+    return (<div className="App">App</div>)
+  }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    posts: state.posts
+  }
+}
+
+const connectedAppComponent=connect(mapStateToProps)(App)
+
+export default connectedAppComponent;
+
+
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux'; 
 import App from './components/App';
 import { configureStore } from './store';
 
-const store = configureStore;
+
+const store = configureStore();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
