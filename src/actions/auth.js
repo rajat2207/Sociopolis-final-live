f3476c9e1cad3f71398838beae818bbe009a2469
rajat2207 <66623568+rajat2207@users.noreply.github.com>
2020-08-09 17:06:09 +0530

    Complete Signup

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,4 +1,11 @@
-import { LOGIN_START, LOGIN_FAILED, LOGIN_SUCCESS } from './actionTypes';
+import {
+  LOGIN_START,
+  LOGIN_FAILED,
+  LOGIN_SUCCESS,
+  SIGNUP_START,
+  SIGNUP_FAILED,
+  SIGNUP_SUCCESS,
+} from './actionTypes';
 import { APIUrls } from '../helpers/urls';
 import { getFormBody } from '../helpers/utils';
 
@@ -11,14 +18,14 @@ export function startLogin() {
 export function loginFailed(errorMessage) {
   return {
     type: LOGIN_FAILED,
-    error: errorMessage
-  }
+    error: errorMessage,
+  };
 }
 
 export function loginSuccess(user) {
   return {
     type: LOGIN_SUCCESS,
-    user
+    user,
   };
 }
 
@@ -43,5 +50,52 @@ export function login(email, password) {
         }
         dispatch(loginFailed(data.message));
       });
+  };
+}
+
+export function signup(email, password, confirmPassword, name) {
+  return (dispatch) => {
+    const url = APIUrls.signup();
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: getFormBody({
+        email,
+        password,
+        confirm_password: confirmPassword,
+        name,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          localStorage.setItem('token', data.data.token);
+          dispatch(signupSuccessful(data.data.user));
+          return;
+        }
+        dispatch(signupFailed(data.message));
+      });
+  };
+}
+
+export function startSignup() {
+  return {
+    type: SIGNUP_START,
+  };
+}
+
+export function signupFailed(error) {
+  return {
+    type: SIGNUP_FAILED,
+    error,
+  };
+}
+
+export function signupSuccessful(user) {
+  return {
+    type: SIGNUP_SUCCESS,
+    user,
   };
 }
--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,35 +1,44 @@
-
-import { LOGIN_START, LOGIN_FAILED, LOGIN_SUCCESS } from '../actions/actionTypes'
+import {
+  LOGIN_START,
+  LOGIN_FAILED,
+  LOGIN_SUCCESS,
+  SIGNUP_FAILED,
+  SIGNUP_START,
+  SIGNUP_SUCCESS,
+} from '../actions/actionTypes';
 
 const initialAuthState = {
-    user: {},
-    error: null,
-    isLoggedIn: false,
-    inProgress: false
-}
+  user: {},
+  error: null,
+  isLoggedIn: false,
+  inProgress: false,
+};
 
 export default function auth(state = initialAuthState, action) {
-    switch (action.type) {
-      case LOGIN_START:
-        return {
-          ...state,
-          inProgress: true,
-        };
-      case LOGIN_SUCCESS:
-        return {
-          ...state,
-          user: action.user,
-          isLoggedIn: true,
-          error: null,
-          inProgress: false
-        };
-      case LOGIN_FAILED:
-        return {
-          ...state,
-          inProgress: false,
-          error: action.error,  
-        };
-      default:
-        return state;
-    }
+  switch (action.type) {
+    case LOGIN_START:
+    case SIGNUP_START:
+      return {
+        ...state,
+        inProgress: true,
+      };
+    case LOGIN_SUCCESS:
+    case SIGNUP_SUCCESS:
+      return {
+        ...state,
+        user: action.user,
+        isLoggedIn: true,
+        error: null,
+        inProgress: false,
+      };
+    case LOGIN_FAILED:
+    case SIGNUP_FAILED:
+      return {
+        ...state,
+        inProgress: false,
+        error: action.error,
+      };
+    default:
+      return state;
+  }
 }
