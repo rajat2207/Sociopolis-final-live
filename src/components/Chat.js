9a4c82ae1440d9f8081b3d7a9e1814ec0a3306a6
rajat2207 <66623568+rajat2207@users.noreply.github.com>
2020-08-18 01:22:08 +0530

    Add Chat Engine

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,20 +1,76 @@
 import React, { Component } from 'react';
+import io from 'socket.io-client';
+import { connect } from 'react-redux';
 
 class Chat extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      messages: [], //{content:'some message', self:true }
+      messages: [], // {content: 'some message', self: true}
       typedMessage: '',
     };
+    this.socket = io.connect('http://54.237.158.65:5000');
+    this.userEmail = props.user.email;
+
+    if (this.userEmail) {
+      this.setupConnections();
+    }
   }
+
+  setupConnections = () => {
+    const socketConnection = this.socket;
+    const self = this;
+
+    this.socket.on('connect', function () {
+      console.log('CONNECTION ESTABLISHED');
+
+      socketConnection.emit('join_room', {
+        user_email: this.userEmail,
+        chatroom: 'codeial',
+      });
+
+      socketConnection.on('user_joined', function (data) {
+        console.log('NE USER JOINED', data);
+      });
+    });
+
+    this.socket.on('receive_message', function (data) {
+      // add message to state
+      const { messages } = self.state;
+      const messageObject = {};
+      messageObject.content = data.message;
+
+      if (data.user_email === self.userEmail) {
+        messageObject.self = true;
+      }
+
+      self.setState({
+        messages: [...messages, messageObject],
+        typedMessage: '',
+      });
+    });
+  };
+
+  handleSubmit = () => {
+    const { typedMessage } = this.state;
+
+    if (typedMessage && this.userEmail) {
+      this.socket.emit('send_message', {
+        message: typedMessage,
+        user_email: this.userEmail,
+        chatroom: 'codeial',
+      });
+    }
+  };
+
   render() {
     const { typedMessage, messages } = this.state;
+
     return (
       <div className="chat-container">
         <div className="chat-header">
-          Group Chat
+          Chat
           <img
             src="https://www.iconsdb.com/icons/preview/white/minus-5-xxl.png"
             alt=""
@@ -40,11 +96,16 @@ class Chat extends Component {
             value={typedMessage}
             onChange={(e) => this.setState({ typedMessage: e.target.value })}
           />
-          <button onClick={this.handleSubmit}>Send</button>
+          <button onClick={this.handleSubmit}>Submit</button>
         </div>
       </div>
     );
   }
 }
 
-export default Chat;
+function mapStateToProps({ auth }) {
+  return {
+    user: auth.user,
+  };
+}
+export default connect(mapStateToProps)(Chat);
